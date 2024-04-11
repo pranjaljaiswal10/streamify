29640899026cac722b36fc562ecd9906a8e8c1bb
Pranjal Jaiswal <pranjaljaiswal35@gmail.com>
2024-04-11 16:38:59 +0530

    feat:add more side bar menu

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,32 +2,23 @@ import { useSelector } from "react-redux";
 
 const SideBar = () => {
     const toggleBar=useSelector((store)=>store.menu.toggleMenu)
+    const keyword=["Shpping","Music","Movies","Live","Gaming","Live","Gaming","News","Sport","Courses","Fashion & Beauty","Podcasts"]
   return (
     toggleBar && <div className="px-6 w-48  ">
     <div className="space-y-3 mt-4">
-        <h1 className="font-bold ">Home</h1>
         <ul>
+        <li className="font-bold ">Home</li>
         <li className="font-medium">Short</li>
         <li className="font-medium">Videos</li>
         <li className="font-medium">Live</li>
         </ul>
     </div>
     <div className="space-y-3 mt-4">
-        <h1 className="font-bold text-xl">Subscriptions</h1>
+        <h1 className="font-bold text-xl">Explore</h1>
         <ul>
-            <li className="font-medium">Music</li>
-            <li className="font-medium">Sports</li>
-            <li className="font-medium">Gaming</li>
-            <li className="font-medium">Movies</li>
-        </ul>
-    </div>
-    <div className="space-y-2 mt-4">
-        <h1 className="font-bold text-xl">Watch later</h1>
-        <ul>
-            <li className="font-medium">Music</li>
-            <li className="font-medium">Sports</li>
-            <li className="font-medium">Gaming</li>
-            <li className="font-medium">Movies</li>
+            {
+                keyword.map((item)=><li key={item} className="font-medium">item</li>)
+            }
         </ul>
     </div>
     </div>
