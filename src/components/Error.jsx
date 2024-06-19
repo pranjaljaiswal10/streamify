0ea46950d187fd632860d95707801b2c98d2aa95
Pranjal Jaiswal <pranjaljaiswal35@gmail.com>
2024-06-19 13:23:36 +0530

    feat:add not found page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,11 +2,11 @@ import { useRouteError } from "react-router-dom";
 
 
 const Error = () => {
-    const err=useRouteError()
-    console.log(err)
+    const error=useRouteError()
+    console.error(error)
   return(<>
-  
-  </>) ;
+    <p>There is nothing here: 404!</p>
+       </>) ;
 };
 
 export default Error;
